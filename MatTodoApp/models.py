4e2f6dc085b31301df92b98d8408471e6dce160a
Matt Swatzell <matt@mattswatzell.com>
2016-06-27 12:03:55 -0400

    Adding new category functionality using classviews

--- MatTodoApp/models.py
+++ MatTodoApp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 import django.contrib.auth.models
 import datetime
+from django.core.urlresolvers import reverse
 
 class Category(models.Model):
     category = models.CharField(max_length=50)
     ownedBy = models.ForeignKey(django.contrib.auth.models.User)
     def __str__(self):
         return self.category
+
+    def get_absolute_url(self):
+        return reverse('index')
     class Meta:
         ordering = ['category']
 
@@ -17,13 +21,13 @@ class ToDoItem(models.Model):
         return datetime.datetime.now() + datetime.timedelta(days=7)
 
     owner = models.ForeignKey(django.contrib.auth.models.User)
-    category = models.ForeignKey(Category, blank=True,  null=True)
+    category = models.ForeignKey(Category, blank=False)
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=200, blank=True)
     comments = models.CharField(max_length=200, blank=True)
     createdDate = models.DateField(auto_now_add=True)
     lastUpdatedDate = models.DateField(auto_now_add=True)
-    nextUpdateDate = models.DateField()
+    nextUpdateDate = models.DateField(verbose_name="Next Update Date")
     dropDeadDate = models.DateField( verbose_name="Due Date")
     priority = models.IntegerField()
     def __str__(self):
--- MatTodoApp/views.py
+++ MatTodoApp/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import redirect, get_object_or_404
-import datetime
+from django.views.generic import ListView, CreateView, UpdateView
 from django.contrib.auth.decorators import login_required
 from MatTodoApp.models import ToDoItem, Category
 from MatTodoApp.forms import ToDoItemForm, CategoryForm
 
 # Create your views here.
 def index(request, errors=[]):
-    errors = {}
+    has_error = False
     if not request.user.is_authenticated():
         return render(request, 'MatTodoApp/index_unauthenticated.html')
     else:
         if request.session.__contains__("errors"):
-            errors["errors"] =  request.session["errors"]
-            request.session.__delitem__("errors")
-        return render(request, 'MatTodoApp/index_authenticated.html', errors)
+            has_error=request.session['errors']
+            request.session.__delitem__('errors')
+        return render(request, 'MatTodoApp/index_authenticated.html', {'errors':has_error})
 
 def register(request):
     if request.method == 'POST':
@@ -31,21 +31,27 @@ def register(request):
 @login_required(login_url='/')
 def addToDo(request):
     if request.method =="POST":
-        form = ToDoItemForm(request.POST, user=request.user)
+        form = ToDoItemForm(request.POST)
+        form.fields["category"].queryset = request.user.category_set.all()
         if form.is_valid():
             item = form.save(commit=False)
             item.owner = request.user
             item.save()
             return redirect('index')
     else:
-        form = ToDoItemForm(user=request.user)
+        queryset  = request.user.category_set.all()
+        if not queryset:
+            request.session["errors"] = "Must add cat"
+            return redirect('index')
+        form = ToDoItemForm()
+        form.fields["category"].queryset = queryset
     return render(request,'MatTodoApp/createToDo.html', {'form':form} )
 
 @login_required(login_url='/')
 def modifyToDo(request,key):
     item = get_object_or_404(ToDoItem, id=key)
-    form = ToDoItemForm(request.POST or None, instance=item, user=request.user)
-    form.category.queryset = request.user.category_set
+    form = ToDoItemForm(request.POST or None, instance=item)
+
     if form.is_valid():
         item = form.save()
         return redirect('index')
@@ -53,12 +59,12 @@ def modifyToDo(request,key):
 
 @login_required(login_url='/')
 def deleteToDo(request, key):
-    error_messages = []
+    error_messages = ""
     try:
         item = ToDoItem.objects.get(id=key)
         item.delete()
     except ToDoItem.DoesNotExist:
-        error_messages.append("Item Does not exist for %s" % key)
+        error_messages=("Item %s does not exist" % key)
     if error_messages:
         request.session["errors"]= error_messages
     return redirect('index')
@@ -75,6 +81,28 @@ def addCat(request):
             return redirect('index')
     else:
         form = CategoryForm()
-
     return render(request, 'MatToDoApp/addCategory.html', {'form':form})
 
+
+class CategoryList(ListView):
+    model = Category
+    def get_context_data(self, **kwargs):
+        return super(CategoryList, self).get_context_data(**kwargs)
+
+    def get_queryset(self):
+        return  self.request.user.category_set.all()
+
+
+class AddCat(CreateView):
+    template_name = 'MatTodoApp/AddCategory.html'
+    model = Category
+    fields = ['category']
+
+    def form_valid(self, form):
+        form.instance.ownedBy = self.request.user
+        return super(AddCat, self).form_valid(form)
+
+class ModCat(UpdateView):
+    template_name = 'MatTodoApp/AddCategory.html'
+    model = Category
+    fields = ['category']
